40087ddea6d0beabfe7cf230827e262b4af6b90b
b-priscacarmel <b.priscacarmel@gmail.com>
2026-04-03 15:00:38 +0530

    backend_setup_created

---
+++ Backend/texteditor/src/test/java/hclguvi/texteditor/TexteditorApplicationTests.java
@@ -0,0 +1,13 @@
+package hclguvi.texteditor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TexteditorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
